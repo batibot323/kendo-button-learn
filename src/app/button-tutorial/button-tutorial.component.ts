0ded8a6c5e1a70d84a4bf12170d451e1821721a7
batibot323 <hani.herbert.ho@gmail.com>
2019-12-16 20:46:12 +0800

    Added kendo-buttongroup. Able to add colors to each button in the button-group using DOM manipulation.

--- src/app/button-tutorial/button-tutorial.component.ts
+++ src/app/button-tutorial/button-tutorial.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { style } from '@angular/animations';
 
 @Component({
   selector: 'kb-button-tutorial',
@@ -6,6 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./button-tutorial.component.css']
 })
 export class ButtonTutorialComponent implements OnInit {
+  changeButtonsToBlue(): void {
+    console.log("To blue clicked!");
+    var buttons = document.getElementById('kendo-btn-grp-1').children;
+    // Can't get this to work. This is different from specifing look="flat" in .html.
+    document.getElementById('kendo-btn-grp-1').setAttribute("ng-reflect-look", "flat");
+    var btnArray = Array.from(buttons);
+    btnArray.forEach(btn => {
+      btn.setAttribute("style", "color: blue");
+    })
+  }
+
+  changeButtonsToBrown(): void {
+    console.log("To brown clicked!");
+    var buttons = document.getElementById('kendo-btn-grp-1').children;
+    var btnArray = Array.from(buttons);
+    btnArray.forEach(btn => {
+      btn.setAttribute("style", "color: brown");
+    })
+  }
 
   constructor() { }
 
