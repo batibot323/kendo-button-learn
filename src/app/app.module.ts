6c80f018a42c825d753a625dadfff5f7fa1543c6
batibot323 <hani.herbert.ho@gmail.com>
2019-12-16 15:35:37 +0800

    Initial Commit. Added button-tutorial-component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ButtonsModule } from '@progress/kendo-angular-buttons';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ButtonTutorialComponent } from './button-tutorial/button-tutorial.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ButtonTutorialComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ButtonsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/button-tutorial/button-tutorial.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'kb-button-tutorial',
+  templateUrl: './button-tutorial.component.html',
+  styleUrls: ['./button-tutorial.component.css']
+})
+export class ButtonTutorialComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
